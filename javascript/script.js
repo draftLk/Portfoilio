00ba23a181515cd5d1d595c0bf17a3597c106707
draftLk <lukasdoczr@gmail.com>
2024-01-14 04:48:13 -0300

    space

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ if (window.SimpleAnime) {
 // SCROLL ANIMADO
 
 const section = document.querySelector('[data-scrollanime=""]');
-const windowMetade = window.innerHeight * 0.5;
+const windowMetade = window.innerHeight * 0.6;
 
 function activeScrollAnimado() {
   const sectionTop = section.getBoundingClientRect().top - windowMetade;
